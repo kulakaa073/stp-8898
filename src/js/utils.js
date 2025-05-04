9b5b39abd53f42528f268150895b30ca25ec323a
Shade <kulakaa073@gmail.com>
2025-05-04 08:55:49 +0300

    tweaks
    Making header background darker for better readability

--- src/js/utils.js
+++ src/js/utils.js
@@ -14,7 +14,6 @@ document.addEventListener('DOMContentLoaded', () => {
 // Add scroll dataset so we can change style on scroll
 document.addEventListener('DOMContentLoaded', () => {
   const header = document.querySelector('header');
-
   window.addEventListener('scroll', () => {
     if (window.scrollY > 0) {
       header.dataset.scrolled = 'true';
@@ -27,7 +26,6 @@ document.addEventListener('DOMContentLoaded', () => {
 // Adjust scroll amount for anchor links, so header wont hide section titles
 document.addEventListener('DOMContentLoaded', () => {
   const links = document.querySelectorAll('a[data-link="anchor"]');
-  const burgerMenuEl = document.querySelector('[data-visible]');
   links.forEach(link => {
     link.addEventListener('click', event => {
       event.preventDefault();
